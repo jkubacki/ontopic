57ef042ca1d299bb1ccf8a783f2d03315d43bb5d
Jakub Kubacki <kubacki.jk@gmail.com>
2016-12-10 14:32:50 +0100

    Add input to chat

--- web/static/js/components/chat/chat_input.js
+++ web/static/js/components/chat/chat_input.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
 export default class ChatInput extends React.Component {
+  _sendMessage(e) {
+    e.preventDefault();
+
+    const { messageInput } = this.refs;
+    const message = messageInput.value;
+    console.log(message);
+  }
+
   render() {
     return (
       <div>
-        --> ChatInput
+        <form onSubmit={::this._sendMessage}>
+          <input type="text" ref="messageInput" />
+          <button type="submit">Send</button>
+        </form>
       </div>
     )
   }
