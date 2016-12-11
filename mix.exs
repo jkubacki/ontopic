6909d10b427438ce3332ac5d59ec96825c436972
Jakub Kubacki <kubacki.jk@gmail.com>
2016-12-11 20:31:07 +0100

    Upgrade deps

--- mix.exs
+++ mix.exs
@@ -18,7 +18,7 @@ defmodule Ontopic.Mixfile do
   # Type `mix help compile.app` for more information.
   def application do
     [mod: {Ontopic, []},
-     applications: [:phoenix, :phoenix_html, :cowboy, :logger, :gettext,
+     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
                     :phoenix_ecto, :postgrex, :comeonin]]
   end
 
@@ -30,16 +30,16 @@ defmodule Ontopic.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.1.4"},
+    [{:phoenix, "~> 1.2.1"},
+     {:phoenix_pubsub, "~> 1.0"},
+     {:phoenix_ecto, "~> 3.0"},
      {:postgrex, ">= 0.0.0"},
-     {:phoenix_ecto, "~> 2.0"},
-     {:phoenix_html, "~> 2.4"},
+     {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.9"},
+     {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
      {:comeonin, "~> 2.0"},
-     {:guardian, "~> 0.9.0"}
-    ]
+     {:guardian, "~> 0.9.0"}]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
